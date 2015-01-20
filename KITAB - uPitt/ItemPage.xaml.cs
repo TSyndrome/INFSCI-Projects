2f9e58ab6d11e4d0ab77d7ce675ee34f0e20394b
TSyndrome <omarhassan2@gmail.com>
2015-01-20 13:51:04 -0500

    Hell

--- KITAB - uPitt/ItemPage.xaml.cs
+++ KITAB - uPitt/ItemPage.xaml.cs
@@ -14,7 +14,7 @@ using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Popop;
 using Windows.UI.Xaml.Navigation;
 
 // The Pivot Application template is documented at http://go.microsoft.com/fwlink/?LinkID=391641
